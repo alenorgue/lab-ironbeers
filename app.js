ae6939e37f396056c6b8295fcd4d5ab5d1e66e87
alenorgue <alenoriegag@gmail.com>
2025-05-27 12:42:32 +0200

    finisehd mandatory iterations, missing Bonus

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const exphbs = require('express-handlebars');
+const { title } = require('process');
+
+
 const app = express();
+
 
 
 const punkAPI = new PunkAPIWrapper();
@@ -28,5 +32,44 @@ hbs.registerPartials(path.join(__dirname, 'views/partials'));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', async (req, res) => {
+  try {
+    const response = await fetch('https://ih-beers-api2.herokuapp.com/beers');
+
+    if (!response.ok) {
+      throw new Error(`Error HTTP: ${response.status}`);
+    }
+
+    const beers = await response.json(); // se convierte manualmente
+    res.render('beers', {
+      title: 'Beer Catalog',
+      beers
+    });
+  } catch (error) {
+    console.error('Error al obtener cervezas:', error);
+    res.status(500).send('Error al cargar las cervezas');
+  }
+});
+
+app.get('/random-beer', async (req, res) => {
+  try {
+    const response = await fetch('https://ih-beers-api2.herokuapp.com/beers/random');
+console.log(response);
+    if (!response.ok) {
+      throw new Error(`Error HTTP: ${response.status}`);
+    }
+
+    const beer = await response.json(); // se convierte manualmente
+    res.render('random-beer', {
+        beer
+
+    });
+  } catch (error) {
+    console.error('Error al obtener cerveza:', error);
+    res.status(500).send('Error al cargar las cerveza');
+  }
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
