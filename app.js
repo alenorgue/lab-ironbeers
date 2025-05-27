955cf375bf021371b9879ea4135e06d398b79aa8
alenorgue <alenoriegag@gmail.com>
2025-05-27 11:26:42 +0200

    first 2 iterations, basic css

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const express = require('express');
-
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
+const exphbs = require('express-handlebars');
 const app = express();
-const punkAPI = new PunkAPIWrapper();
 
+
+const punkAPI = new PunkAPIWrapper();
+app.engine('hbs', exphbs.engine({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+
+// Register layout.hbs as the main layout
+defaultLayout = path.join(__dirname, 'views/layout/layout.hbs');
+
+app.set('view options', { layout: defaultLayout });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 
 // Add the route handlers here:
 
